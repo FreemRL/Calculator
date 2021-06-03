47e6e6706b9b2884f67759746ca196b3aeea3f7c
FreemRL <66525499+FreemRL@users.noreply.github.com>
2021-06-03 20:15:13 +0200

    testFix

--- commands/calculate.js
+++ commands/calculate.js
@@ -1,14 +1,16 @@
-const { create, all } = require('mathjs');
-const math = create(all);
-const limitedEval = math.evaluate;
+import { create, all } from 'mathjs'
+
+const math = create(all)
+const limitedEvaluate = math.evaluate
+
 math.import({
-    'import':     function () { throw new Error('Function import is disabled') },
-    'createUnit': function () { throw new Error('Function createUnit is disabled') },
-    'evaluate':   function () { throw new Error('Function evaluate is disabled') },
-    'parse':      function () { throw new Error('Function parse is disabled') },
-    'simplify':   function () { throw new Error('Function simplify is disabled') },
-    'derivative': function () { throw new Error('Function derivative is disabled') }
-  }, { override: true })
+  'import':     function () { throw new Error('Function import is disabled') },
+  'createUnit': function () { throw new Error('Function createUnit is disabled') },
+  'evaluate':   function () { throw new Error('Function evaluate is disabled') },
+  'parse':      function () { throw new Error('Function parse is disabled') },
+  'simplify':   function () { throw new Error('Function simplify is disabled') },
+  'derivative': function () { throw new Error('Function derivative is disabled') }
+}, { override: true })
 
 const Discord = require('discord.js');
 
