55257964a83fb04dc90b387716037cf79bbecbc1
FreemRL <66525499+FreemRL@users.noreply.github.com>
2021-06-03 20:27:52 +0200

    sgfsgdh

--- commands/calculate.js
+++ commands/calculate.js
@@ -10,7 +10,8 @@ math.import({
     'evaluate': function () { throw new Error('Function evaluate is disabled') },
     'parse': function () { throw new Error('Function parse is disabled') },
     'simplify': function () { throw new Error('Function simplify is disabled') },
-    'derivative': function () { throw new Error('Function derivative is disabled') }
+    'derivative': function () { throw new Error('Function derivative is disabled') },
+    'range': function() { throw new Error('Function range is disabled.') }
 }, { override: true })
 
 module.exports = {
